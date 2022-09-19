03fb96bbc4a5c9f67c5b7849e3fd9bd91676aa55
carlos.castaneda.mt@usa.edu.co <carlos.castaneda.mt@usa.edu.co>
2022-09-19 10:57:13 -0500

    proyecto mi primer commit

---
+++ src/main/java/com/mycompany/proyectogit/Ejercicio1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.proyectogit;
+
+/**
+ *
+ * @author Itsha Suarez
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hola mundo git");
+        
+    }
+    
+}
